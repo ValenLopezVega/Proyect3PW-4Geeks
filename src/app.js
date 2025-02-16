edb9f7f998235b0358fbdddf6e9bed1bd1576312
Valentina López Vega <37059478+ValenLopezVega@users.noreply.github.com>
2025-02-16 22:41:10 +0000

    Proyect 3 finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
+ window.onload = function() {
+  function generateExcuse() {
+      let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+      let action = ['ate', 'peed', 'crushed', 'broke'];
+      let what = ['my homework', 'my phone', 'the car'];
+      let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+       let whoIndex = Math.floor(Math.random() * who.length);
+       let actionIndex = Math.floor(Math.random() * action.length);
+       let whatIndex = Math.floor(Math.random() * what.length);
+       let whenIndex = Math.floor(Math.random() * when.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+       let excuse = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}.`;
+       document.getElementById("excuse").innerHTML = excuse;
+   }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+   generateExcuse();
+ };
